4ab5f69b8ecafdc249a85c2f0e03d263aa2a16ed
DiegoFHG <34254918+DiegoFHG@users.noreply.github.com>
2023-08-23 22:32:15 +0200

    (feature) Containerized project and added more services

--- config.py
+++ config.py
@@ -1,12 +1,16 @@
+import psycogreen.gevent
 from flask import Flask
 from flask_marshmallow import Marshmallow
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_cors import CORS
 
+# Patch PostgreSQL driver for Gunicorn gevent (async)
+psycogreen.gevent.patch_psycopg()
+
 app = Flask('ecommerce')
 
-app.config["SQLALCHEMY_DATABASE_URI"] = "postgresql://ecommerce:ecommerce@localhost:5432/ecommerce"
+app.config["SQLALCHEMY_DATABASE_URI"] = "postgresql://ecommerce:ecommerce@ecommerce-db:5432/ecommerce"
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db = SQLAlchemy(app)
